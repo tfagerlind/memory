d34c06c15f8139a727b745feeb6e58cc79dd0a1b
Tomas Fägerlind <tomas.fagerlind@sofengi.com>
2024-03-22 22:53:37 +0100

    refactor: break out method

--- pi-matrix.py
+++ pi-matrix.py
@@ -10,10 +10,13 @@ group_indices = [index for index in range(30)]
 
 random.shuffle(group_indices)
 
-while group_indices:
-    group_index = group_indices.pop()
-    group = [nth_decimal(1 + group_index * 5 + decimal_index)
+def get_group(index):
+    return [nth_decimal(1 + index * 5 + decimal_index)
             for decimal_index
             in range(5)]
+
+while group_indices:
+    index = group_indices.pop()
+    group = get_group(index=index)
     group_as_string = "".join(group)
     print(group_as_string)
